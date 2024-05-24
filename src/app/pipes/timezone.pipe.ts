fdee2ce874a81af4f90eae5060ae65ace7e81fb9
Raymond Gigliotti <ray@firestitch.com>
2024-05-25 00:20:48 +0300

    added fsTimezone pipe

---
+++ src/app/pipes/timezone.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { FsTimezone } from '../services';
+
+
+@Pipe({ name: 'fsTimezone' })
+export class FsTimezonePipe implements PipeTransform {
+  
+  constructor(
+    private _timezone: FsTimezone,
+  ) {}
+
+  public transform(identifier: string): string {
+
+    const timezone = this._timezone.timezones
+      .find((item) => item.identifier === identifier);
+
+    return timezone?.description || '';
+  }
+}
--- src/public_api.ts
+++ src/public_api.ts
@@ -2,5 +2,6 @@ export { FsTimezoneModule } from './app/fs-timezone.module';
 
 export * from './app/components';
 
+export * from './app/pipes';
 export * from './app/services';
 
