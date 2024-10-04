ddce09d1be8afdd3f122ab40c1569d963d20bfa9
Raymond Gigliotti <ray@firestitch.com>
2024-10-04 06:05:17 -0400

    fixed demo site

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FsExampleModule } from '@firestitch/example';
 import { FsLabelModule } from '@firestitch/label';
 import { FsMessageModule } from '@firestitch/message';
 import { FsStoreModule } from '@firestitch/store';
-
-import { ToastrModule } from 'ngx-toastr';
-
 import { FsTimezoneModule } from '@firestitch/timezone';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import {
   ExamplesComponent,
-  TimezoneComponent
+  TimezoneComponent,
 } from './components';
 import { AppMaterialModule } from './material.module';
 
@@ -25,7 +24,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -36,7 +35,6 @@ const routes: Routes = [
     FsExampleModule.forRoot(),
     FsTimezoneModule,
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes),
   ],
   declarations: [
